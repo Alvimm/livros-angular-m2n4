7e642e96604494f59583312b8253c199364bed97
Filipe Alvim <filipe2012alvim@gmail.com>
2023-04-27 19:00:05 -0300

    add the LivroDadosComponent

--- src/app/livro-dados/livro-dados.component.ts
+++ src/app/livro-dados/livro-dados.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Livro } from '../livro';
+import { Editora } from '../editora';
+import { ControleEditoraService } from '../controle-editora.service';
+import { ControleLivrosService } from '../controle-livros.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-livro-dados',
   templateUrl: './livro-dados.component.html',
   styleUrls: ['./livro-dados.component.css']
 })
-export class LivroDadosComponent {
+export class LivroDadosComponent implements OnInit {
+  livro:Livro
+  autoresForm: string = ''
+  editoras: Array<Editora> = []
+  servEditora: ControleEditoraService
+  servLivros: ControleLivrosService
 
+  constructor(livro:Livro, servEditora: ControleEditoraService, servLivros: ControleLivrosService, private router: Router) {
+    this.livro = livro
+    this.servEditora = servEditora
+    this.servLivros = servLivros
+    this.router = router
+  }
+
+  incluir = () => {
+    this.livro.autores = this.autoresForm.split('\n')
+    this.servLivros.incluir(this.livro)
+    this.router.navigateByUrl('/lista');
+  }
+
+  ngOnInit() {
+    this.editoras = this.servEditora.getEditoras()
+  }
 }
