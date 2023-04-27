0d149304bcfb73994b46188f408bceda2e513071
Filipe Alvim <filipe2012alvim@gmail.com>
2023-04-27 17:29:46 -0300

    start the logical part (services and components)

--- src/app/controle-livros.service.ts
+++ src/app/controle-livros.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { Livro } from './livro';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ControleLivrosService {
-
+  livros: Array<Livro> = [
+    {codigo:1, codEditora:1, titulo:'Livro 1', resumo:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem.', autores:['José Augusto', 'Lucia Ramires'] },
+    {codigo:2, codEditora:2, titulo:'Livro 2', resumo:'Suspendisse potenti. Maecenas non velit auctor, eleifend urna sed, dignissim felis. Quisque aliquet viverra ipsum eu bibendum.', autores:['Paulo Coelho', 'Pat Johnson'] },
+    {codigo:3, codEditora:3, titulo:'Livro 3', resumo:'Proin venenatis turpis vel ipsum bibendum, quis aliquam lectus accumsan. Sed id leo in nisi ornare iaculis ut in nulla.', autores:['Maria Dagostini', 'Liandra Smith'] },
+    {codigo:4, codEditora:4, titulo:'Livro 4', resumo:'teste de resumo.', autores:['João Dagostini', 'Paul Smith'] },
+  ]
   constructor() { }
+
+  obterLivros():Array<Livro>{
+    return this.livros
+  }
+
+  incluir(livro:Livro):void{
+    const maiorCodigo = this.livros[this.livros.length - 1].codigo
+    livro.codigo = maiorCodigo + 1
+    this.livros.push(livro)
+  }
+
+  excluir(codigo:number):void{
+    const index = this.livros.findIndex((livro)=> livro.codigo === codigo)
+    if(index >= 0) this.livros.splice(index, 1)
+  }
 }
--- src/app/livro-lista/livro-lista.component.ts
+++ src/app/livro-lista/livro-lista.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Editora } from '../editora';
+import { Livro } from '../livro';
+import { ControleEditoraService } from '../controle-editora.service';
+import { ControleLivrosService } from '../controle-livros.service';
 
 @Component({
   selector: 'app-livro-lista',
   templateUrl: './livro-lista.component.html',
   styleUrls: ['./livro-lista.component.css']
 })
-export class LivroListaComponent {
+export class LivroListaComponent implements OnInit {
+  editoras: Array<Editora> = []
+  livros: Array<Livro> = []
+  servEditora: ControleEditoraService
+  servLivros: ControleLivrosService
 
+  constructor(servEditora: ControleEditoraService, servLivros: ControleLivrosService) {
+    this.servEditora = servEditora
+    this.servLivros = servLivros
+  }
+
+  ngOnInit():void {
+    this.editoras = this.servEditora.getEditoras()
+    this.livros = this.servLivros.obterLivros()
+  }
 }
